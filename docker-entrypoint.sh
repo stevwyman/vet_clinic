ca620823ddb072628ef0570e28fbf233f4cc25ce
Stev Wyman <stev.wyman@gmail.com>
2023-11-29 17:01:53 +0100

    correcting path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,6 @@ django-admin makemessages --all --ignore=env
 django-admin compilemessages --ignore=env 
 echo "... translations compiled"
 
-python manage.py migrate
+python vet/manage.py migrate
 
 exec "$@"
